b723375236128c1dc1e35701351467ad23f0dcb6
mpchow <mattchow918@gmail.com>
2020-02-07 09:30:22 -0800

    Changed to simpleio

--- buzzerTest.py
+++ buzzerTest.py
@@ -1,34 +1,24 @@
-from time import sleep
 import board
-import pulseio
 
-# For the M4 boards:
-piezo = pulseio.PWMOut(board.A1, duty_cycle=0, frequency=440, variable_frequency=True)
- 
-while True:
-    for f in (262, 294, 330, 349, 392, 440, 494, 523):
-        piezo.frequency = f
-        piezo.duty_cycle = 65536 // 2  # On 50%
-        time.sleep(0.25)  # On for 1/4 second
-        piezo.duty_cycle = 0  # Off
-        time.sleep(0.05)  # Pause between notes
-    time.sleep(0.5)
+import simpleio
 
 
+# Define pin connected to piezo buzzer.
+PIEZO_PIN = board.A1
 
-#Experimental Code to test making different notes
-ON = 2**15
-OFF = 0
-c = 262
-d = 294
-e = 330
-f = 349
+# Define a list of tones/music notes to play.
+c = 262  # C4
+d = 294  # D4
+e = 330  # E4
+f = 349  # F4
+g = 392  # G4
+a = 440  # A4
+b = 494  # B4
 notes = [c, c, d, c, f, e]
-while True:
-    for f in notes:
-        piezo.frequency = f
-        piezo.duty_cycle = ON
-        time.sleep(0.2)
-        piezo.duty_cycle = OFF
-        time.sleep(0.2)
 
+
+# Main loop will go through each tone in order up and down.
+while True:
+    # Play tones going from start to end of list.
+    for note in notes:
+        simpleio.tone(PIEZO_PIN, note, duration=0.5)
