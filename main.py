9c0e4f02968158e3acc1f6fecbc9645ebeb3275b
Matthew Yen <mgyen@shaw.ca>
2020-02-12 09:48:08 -0800

    changing main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 This test will initialize the display using displayio and draw a solid green
 background, a smaller purple rectangle, and some yellow text.
 """
-
+'''
 import board
 import displayio
 import terminalio
@@ -71,6 +71,96 @@ while True:
         piezo.duty_cycle = ON
         sleep(0.3)
         piezo.duty_cycle = OFF
-        sleep(0.3)
+        sleep(0.3)'''
+        
+#Note: TODO Needs to be tested.
+import time
+import board
+import pulseio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+# can change inputs board.A1 etc.
+pwm1 = pulseio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50)
+pwm2 = pulseio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50)
+pwm3 = pulseio.PWMOut(board.D11, duty_cycle=2 ** 15, frequency=50)
+pwm4 = pulseio.PWMOut(board.D5, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+lowLeft = servo.Servo(pwm1) #alternative name servo1
+lowRight = servo.Servo(pwm2) #alternative name servo2
+upperRight = servo.Servo(pwm3) #alternative name servo3
+upperLeft = servo.Servo(pwm4) #alternative name servo4
+
+def dance4():
+    while True:
+        for angle in range(60, 120, 5):  # 30 - 150 degrees, 5 degrees at a time.
+            lowLeft.angle = angle
+            time.sleep(0.01)
+            lowRight.angle = 180 - angle
+            time.sleep(0.01)
+            upperRight.angle = angle
+            time.sleep(0.01)
+            upperLeft.angle = 180 - angle
+            time.sleep(0.01)
+        for angle in range(120, 60, -5):  # 150 - 30 degrees, 5 degrees at a time.
+            lowLeft.angle = angle
+            time.sleep(0.01)
+            lowRight.angle = 180 - angle
+            time.sleep(0.01)
+            upperRight.angle = angle
+            time.sleep(0.01)
+            upperLeft.angle = 180 - angle
+            time.sleep(0.01)
+
+        for angle in range(60, 120, 5):  # 150 - 30 degrees, 5 degrees at a time.
+            if angle % 10 == 0:
+                lowLeft.angle = angle - 10
+                time.sleep(0.02)
+                lowRight.angle = 180 - angle + 10
+                time.sleep(0.02)
+                upperRight.angle = angle - 10
+                time.sleep(0.02)
+                upperLeft.angle = 180 - angle + 10
+                time.sleep(0.02)
+
+            lowLeft.angle = angle
+            time.sleep(0.01)
+            lowRight.angle = 180 - angle
+            time.sleep(0.01)
+            upperRight.angle = angle
+            time.sleep(0.01)
+            upperLeft.angle = 180 - angle
+            time.sleep(0.01)
+
+        for angle in range(120, 60, -5):  # 150 - 30 degrees, 5 degrees at a time.
+            if angle % 10 == 0:
+                lowLeft.angle = angle - 20
+                time.sleep(0.01)
+                lowRight.angle = 180 - angle + 20
+                time.sleep(0.01)
+                upperRight.angle = angle - 20
+                time.sleep(0.01)
+                upperLeft.angle = 180 - angle + 20
+                time.sleep(0.01)
+
+            lowLeft.angle = angle
+            time.sleep(0.15)
+            lowRight.angle = 180 - angle
+            time.sleep(0.15)
+            upperRight.angle = angle
+            time.sleep(0.15)
+            upperLeft.angle = 180 - angle
+            time.sleep(0.15)
+
+        """
+        if some command break
+        """
+    #default position
+    lowLeft.angle = 90
+    lowRight.angle = 90
+    upperLeft.angle = 90
+    upperRight.angle = 90
 
 
+dance4()
