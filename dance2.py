fbb61717978b728ff9560efbe76382e556c89390
Sebastian Gonzalez <sebastian.gonzalez@alumni.ubc.ca>
2020-02-12 09:00:51 -0800

    Michael Jackson wishes he could dance like this.

--- dance2.py
+++ dance2.py
@@ -1,12 +1,14 @@
+#Note: TODO Needs to be tested.
 import time
 import board
 import pulseio
 from adafruit_motor import servo
 
 # create a PWMOut object on Pin A2.
-pwm1 = pulseio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency = 50)
+# can change inputs board.A1 etc.
+pwm1 = pulseio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50)
 pwm2 = pulseio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50)
-pwm3 = pulseio.PWMOut(board.D11, duty_cycle=2 ** 15, frequency = 50)
+pwm3 = pulseio.PWMOut(board.D11, duty_cycle=2 ** 15, frequency=50)
 pwm4 = pulseio.PWMOut(board.D5, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
@@ -15,15 +17,18 @@ lowRight = servo.Servo(pwm2)
 upperRight = servo.Servo(pwm3)
 upperLeft = servo.Servo(pwm4)
 
-while True:
-    for num in range(10):
-        lowLeft.angle = 60
-        lowRight.angle = 60
-        lowLeft.angle = 120
-        lowRight.angle = 120
+timeout = time.time() + 60*5   # 5 minutes from now
 
-    for num in range(15):
-        upperRight.angle = 50
-        upperLeft.angle = 130
-        upperLeft.angle = 50
-        upperRight.angle = 130
+def dance2():
+    while True:
+        for num in range(10):
+            lowLeft.angle = 60
+            lowRight.angle = 60
+            lowLeft.angle = 120
+            lowRight.angle = 120
+
+        for num in range(15):
+            upperRight.angle = 50
+            upperLeft.angle = 130
+            upperLeft.angle = 50
+            upperRight.angle = 130
