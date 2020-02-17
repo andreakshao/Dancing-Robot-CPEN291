b89b7afeaeb7717dd8da7dfa4653baefa97fc885
Andrea Shao <andrea25shao@gmail.com>
2020-02-17 13:06:28 -0800

    Added more sleep

--- dance7.py
+++ dance7.py
@@ -27,16 +27,23 @@ def dance7():
     while True:
         # lift left foot, move right foot, lower left foot
         lowLeft.angle = 120
+        time.sleep(0.2)
         lowRight.angle = 80
+        time.sleep(0.2)
         upRight.angle = 50
+        time.sleep(0.2)
         lowLeft.angle = 90
-        time.sleep(0.1)
+        time.sleep(0.2)
 
         # lift right foot, reset right knee, move left foot, lower right foot 
         lowRight.angle = 120
+        time.sleep(0.2)
         lowLeft.angle = 80
+        time.sleep(0.2)
         upRight.angle = 90
+        time.sleep(0.2)
         upLeft.angle = 50
+        time.sleep(0.2)
         lowRight.angle = 90
-        time.sleep(0.1)
+        time.sleep(0.2)
 dance7()
