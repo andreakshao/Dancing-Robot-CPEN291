833508c6c6e34223f530e0c361950e8b79ffd48c
Sebastian Gonzalez <sebastian.gonzalez@alumni.ubc.ca>
2020-02-17 15:54:27 -0800

    Might work.

--- lcdTest_v2.py
+++ lcdTest_v2.py
@@ -5,19 +5,6 @@ from adafruit_st7735r import ST7735R
 
 from time import sleep
 import board
-import pulseio
-
-# For the M4 boards:
-piezo = pulseio.PWMOut(board.A1, duty_cycle=0, frequency=440, variable_frequency=True)
-
-#Experimental Code to test making different notes
-ON = (2**15)
-OFF = 0
-c = 262
-d = 294
-e = 330
-f = 349
-notes = [c, c, d, c, f, e]
 
 # Release any resources currently in use for the displays
 displayio.release_displays()
@@ -28,16 +15,22 @@ tft_dc = board.D9
 
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=board.D7)
 
-display = ST7735R(display_bus, width=128, height=128, colstart=2, rowstart=1)
+display = ST7735R(display_bus, width=128, height=128)
 
 
 # Make the display context
 splash = displayio.Group(max_size=20)
 
-my_label = label.Label(terminalio.FONT, text="My Label Text", color=0x000000, x = 40, y = 40)
+my_label = label.Label(terminalio.FONT, text="My Label Text", color=0xFFFFFF, x = 40, y = 40)
 splash.append(my_label)
 
 display.show(splash)
+
+text = "Hello world"
+text_area = label.Label(terminalio.FONT, text=text)
+text_area.x = 10
+text_area.y = 10
+board.DISPLAY.show(text_area)
 
 while True:
     pass
