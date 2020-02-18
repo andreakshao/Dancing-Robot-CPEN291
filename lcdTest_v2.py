75b24dcffbd2191df2492c7c391047b6f771f92c
Matthew Yen <mgyen@shaw.ca>
2020-02-17 16:16:34 -0800

    LCD WORKDScd mp_A_G4/cd mp_A_G4/

--- lcdTest_v2.py
+++ lcdTest_v2.py
@@ -29,7 +29,7 @@ text = "Hello world"
 text_area = label.Label(terminalio.FONT, text=text)
 text_area.x = 10
 text_area.y = 10
-board.DISPLAY.show(text_area)
+display.show(text_area)
 
 while True:
     pass
